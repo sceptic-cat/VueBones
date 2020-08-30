5770831fc9f29502179020dc8d27f622aae9b23b
iScepsis <paradoxsepsis@gmail.ru>
2020-08-30 23:47:58 +0300

    Still torture webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //Плагин для автоматического добавления ссылки на сгенерированный бандл
+//const HtmlWebpackPlugin = require('html-webpack-plugin'); //Плагин для автоматического добавления ссылки на сгенерированный бандл
 const {CleanWebpackPlugin} = require('clean-webpack-plugin'); //Плагин автоматической отчистки старых бандлов
 
 module.exports = {
+    context: path.resolve(__dirname, 'public'),
     mode: 'development', //TODO:: продумать как переключаться на бой
     entry: './public/javascripts/main.js',
     output: {
-        filename: '[name].[contenthash].js',
+        //filename: '[name].[contenthash].js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'public/javascripts')
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './views/index.ejs'
-        }),
+      /*  new HtmlWebpackPlugin({
+            template: '!!ejs-compiled-loader!./views/index.ejs',
+            inject: 'body'
+        }),*/
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
